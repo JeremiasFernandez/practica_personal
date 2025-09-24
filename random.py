62466fbe2f10d1683d5d962c063b6b03e3885b3b
Jeremias Fernandez <jereferdz@gmail.com>
2025-09-24 19:50:11 -0300

    Create random.py

---
+++ random.py
@@ -0,0 +1,12 @@
+import random
+
+excuses = [
+    "No fui a clase porque el compilador me odia 💻",
+    "Se me cayó Internet... de nuevo.",
+    "El profe dijo que el bug era 'feature' 🤯",
+    "Estaba entrenando a mi Charizard 🔥🐉",
+    "Git me pidió sacrificios y tardé horas 🙃"
+]
+
+print("Excusa random para hoy:")
+print(random.choice(excuses))
