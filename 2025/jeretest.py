6b9877eba5a01899794fb1e98c9d2db068a25a04
Jeremias Fernandez <jereferdz@gmail.com>
2025-09-23 22:43:41 -0300

    new

---
+++ 2025/jeretest.py
@@ -0,0 +1,109 @@
+# Test de jere jeje
+
+puntos = 0
+
+# Seccion facil
+print("Seccion: Facil \nLos puntos ahora valdran por 2")
+print(" ")
+
+
+# Pregunta 1
+
+while True:
+  pregunta1 = input("Pregunta 1: ¿Como me llamo?: \nOpcion 1: jere \nOpcion 2: Gere \n (Pon 1 o 2): ")
+
+  if pregunta1 == "1":
+    print("Correcto! (menos mal)")
+    puntos += 2
+    break
+  
+  elif pregunta1 == "2":
+    print("incorrecto??")
+    break
+  
+  else:
+    print("q ponia, invalido")
+    print(" ")
+
+# Pregunta 2
+print("  ")
+
+while True:
+  pregunta2 = input("¿Como se llama mi gatita? \nOpcion 1: Helena \nOpcion 2: Cata \nOpcion 3: Gatita")
+  
+  if pregunta2 == "1":
+    print("Incorrecto, se llama gatita (en serio)")
+    break
+  
+  elif pregunta2 == "2":
+    print("Incorrecto, se llama gatita (pero casi)")
+    break
+    
+  elif pregunta2 == "3":
+    print("Correcto!! es el nombre mas original del mundo")
+    puntos += 2
+    break
+  
+  else:
+    print("jajajjjajajjj q gracioso")
+    print(" ")
+    
+# Pregunta 3
+print("    ")
+
+while True:
+  pregunta3 = input("¿Cual es mi instrumento favorito? \nOpcion 1: Guitarra \nOpcion 2: Piano \nOpcion 3: La musica")
+  
+  if pregunta3 == "1":
+    print("Incorrectaa, es el piano")
+    break
+  
+  elif pregunta3 == "2":
+    print("Correcta!! muy ez")
+    puntos += 2
+    break
+  
+  elif pregunta3 == "3":
+    print("BBOEEE, incorrecta, es el piano")
+    break
+  
+  else:
+    print("Elegi aigo dale")
+    
+
+#Pregunta 4
+print("    ")
+
+pregunta4 = input("¿Que estudio? \nOpcion 1: Arquitectura \nOpcion 2: Psicologia \nOpcion 3: Programacion \nOpcion 4: Diseño  \nOpcion 5: Skibidi")
+
+while True:  
+  if pregunta4 == "1":
+    print("Incorrecta! es programacion, aunque funfact: de chiquito queria ser arquirecto")
+    break
+  
+  elif pregunta4 == "2":
+    print("Incorrecta!! Era programacion, aunque a mi mama le hubiese gudtado que sea psicologo")
+    
+  elif pregunta4 == "3":
+    print("Correcta!! q sufrimiento saquenme de aca")
+    puntos += 2 
+    break
+  
+  elif pregunta4 == "4":
+    print("jajjajajajjajjajajjajajjajaaajaa no")
+    break
+
+  elif pregunta4 == "5":
+    print("todas las mañanas skibidii \nskibidi dop dop dop yes yes")
+    break
+
+  else:
+    print("daaale no t hagas el gracioso mira q te mato")
+  
+# Pregunta 5
+
+while True:
+  pregunta5 = input("")
+  
+  
+  
