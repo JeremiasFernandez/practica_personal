446b686315facdbcee1210a40f0a2269a2384c29
Jeremias Fernandez <jereferdz@gmail.com>
2025-09-24 19:51:08 -0300

    Update random.py

--- random.py
+++ random.py
@@ -1,11 +1,11 @@
 import random
 
 excuses = [
-    "No fui a clase porque el compilador me odia 💻",
+    "No fui a clase porque el compilador me odia",
     "Se me cayó Internet... de nuevo.",
-    "El profe dijo que el bug era 'feature' 🤯",
-    "Estaba entrenando a mi Charizard 🔥🐉",
-    "Git me pidió sacrificios y tardé horas 🙃"
+    "El profe dijo que el bug era 'feature'",
+    "Estaba entrenando a mi Charizard",
+    "Git me pidió sacrificios y tardé horas"
 ]
 
 print("Excusa random para hoy:")
